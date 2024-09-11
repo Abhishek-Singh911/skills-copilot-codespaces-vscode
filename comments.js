b3cb1acc776664ec1efe49dcae8e36ca859593d8
Abhishek-Singh911 <abhishek.singh@talentica.com>
2024-09-11 11:46:30 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,3 +1,15 @@
 // create web server
-// create a route for comments
-// print all comments
+// create a server
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// create a server
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// listen to port 3000
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
